fead80c9d49b1c668bed1aefe7a773250eebec99
Elias <32110106+lolmanQ@users.noreply.github.com>
2020-02-05 14:52:50 +0100

    added build

---
+++ my-site/build/precache-manifest.f445f877f53a03e1567c8e1939f3b506.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60158b862b462420bc013af8643bb101",
+    "url": "/index.html"
+  },
+  {
+    "revision": "845db61b0d3d49b7fd99",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d718eb9bc611373d29fd",
+    "url": "/static/js/2.4fc9b968.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.4fc9b968.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845db61b0d3d49b7fd99",
+    "url": "/static/js/main.e6c4851f.chunk.js"
+  },
+  {
+    "revision": "4e81e6700ec56eb4ae23",
+    "url": "/static/js/runtime-main.5eb8e6b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
