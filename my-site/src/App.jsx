4d96eb635ed5edb54ef5d91908a5171db9d54f35
Elias <32110106+lolmanQ@users.noreply.github.com>
2020-02-11 13:51:31 +0100

    backend

--- my-site/src/App.jsx
+++ my-site/src/App.jsx
@@ -6,12 +6,25 @@ import Projects from './components/Projects/Projects.jsx';
 import About from './components/About.jsx';
 import Error from './components/Error';
 import Navigation from './components/Navigation';
+import Amplify, { API } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
  
 class App extends Component {
+	async getData(){
+		let apiName = 'siteAPI';
+    	let path = '/items';
+    	let myInit = { // OPTIONAL
+        	headers: {} // OPTIONAL
+    	}
+    	return await API.get(apiName, path, myInit);
+	}
+	
   render() {
     return (      
        <BrowserRouter>
-        <div>
+        <div>{console.log(this.getData())}
             <Switch>
              <Route path="/" component={Home} exact/>
              <Route path="/Projects" component={Projects}/>
