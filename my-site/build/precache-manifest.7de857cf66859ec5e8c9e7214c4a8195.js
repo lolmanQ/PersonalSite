7a5ad8647e61aa8b8ea4bdfa117fdc4bf20290f3
Elias <32110106+lolmanQ@users.noreply.github.com>
2020-03-12 15:12:52 +0100

    gitignore update

---
+++ my-site/build/precache-manifest.7de857cf66859ec5e8c9e7214c4a8195.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa259c9f010f47eded20e3900f2511b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99c5c1f3383907ef86ee",
+    "url": "/static/css/main.b0c86442.chunk.css"
+  },
+  {
+    "revision": "3fa07715bfae2ae7336c",
+    "url": "/static/js/2.59696648.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.59696648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99c5c1f3383907ef86ee",
+    "url": "/static/js/main.dadae5fd.chunk.js"
+  },
+  {
+    "revision": "4e81e6700ec56eb4ae23",
+    "url": "/static/js/runtime-main.5eb8e6b3.js"
+  },
+  {
+    "revision": "30db56fc52c042b88ee82121685dcb5a",
+    "url": "/static/media/VRChat1.30db56fc.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "94a6f57399b1871cb7ff1bb9032f0541",
+    "url": "/static/media/streetSignsUF.94a6f573.png"
+  }
+]);
